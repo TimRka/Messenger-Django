1e1102f6fc16683f8a0ae042f70d8425ff16ac1f
TimRka <45149604+TimRka@users.noreply.github.com>
2026-05-07 01:12:57 +0300

    OMG I HATE HTML

--- messenger/users/urls.py
+++ messenger/users/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import SignUpView
 from .forms import CustomAuthenticationForm
+from django.views.generic import TemplateView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
-        authentication_form=CustomAuthenticationForm,
         template_name='registration/login.html'
     ), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', TemplateView.as_view(template_name='users/logged_out.html'), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- messenger/users/views.py
+++ messenger/users/views.py
@@ -8,9 +8,10 @@ from .models import CustomUser
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/signup.html'
 
     def form_valid(self, form):
         response = super().form_valid(form)
         return response
+
