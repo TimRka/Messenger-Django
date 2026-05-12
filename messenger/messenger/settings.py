fd769eebd1d172e5f1b80cb6caa5393491e56ad7
TimRka <45149604+TimRka@users.noreply.github.com>
2026-05-12 23:17:08 +0300

    Fix avatars, some detailing in header

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = '/chats/'
 LOGOUT_REDIRECT_URL = '/'
 
